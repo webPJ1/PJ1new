9cb1ae0254b3b7fa62fc87f13bd2652e78e8fc6f
thangkun3012 <thangkun3012@gmail.com>
2017-08-01 12:42:08 +0700

    Update main.php

--- shop xe may/layout/main.php
+++ shop xe may/layout/main.php
@@ -3,15 +3,41 @@
 </div> <!-- end of header_wrapper -->
 
 <div id="Slide">
-<h1>Slide ở đây </h1>
-</div>
-<div id="BHT_middle_wrapper1" class="content">
-	<div id="BHT_middle_wrapper2">
-		<div id="BHT_middle">
+<div class="slideshow-container">
+  <div class="mySlides fade">
+    
+    <img src="images/zero-s-2009-moto.jpeg" style="height:400px; width:1536px">
+    
+  </div>
 
-			<h1>Banner</h1>
-			
-			
-		</div>
-	</div>
+  <div class="mySlides fade">
+    
+    <img src="images/xeee1.jpg" style="height:400px; width:1536px">
+    
+  </div>
+
+  <div class="mySlides fade">
+    
+    <img src="images/io.jpg" style="height:400px; width:1536px">
+    
+  </div>
+</div>
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+
+
 </div>
