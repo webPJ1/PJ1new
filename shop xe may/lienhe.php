1152eb6cd106a0d4793eb9c5be3aeeb69a4b51b8
thangkun3012 <thangkun3012@gmail.com>
2017-08-01 13:04:46 +0700

    Add files via upload

---
+++ shop xe may/lienhe.php
@@ -0,0 +1,17 @@
+<?php 
+include_once"layout/header.php";
+include_once"layout/top.php";
+include_once"layout/menu.php";
+?>
+
+ </div>	  
+</div> <!-- end of header_wrapper -->
+
+<div id="Slide">
+<h1>Liên hệ </h1>
+</div>
+
+<?php 
+include_once"layout/banner.php";
+include_once"layout/footer.php";
+?>
