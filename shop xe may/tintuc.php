bbda78cf69902ed69faf1c4e993dc1821d66b1e1
thangkun3012 <thangkun3012@gmail.com>
2017-08-01 13:03:09 +0700

    Update tintuc.php

--- shop xe may/tintuc.php
+++ shop xe may/tintuc.php
@@ -1,1 +1,23 @@
-<div id="Slide">Đây là trang tin tức</div>
+<?php 
+include_once"layout/header.php";
+include_once"layout/top.php";
+include_once"layout/menu.php";
+?>
+
+ </div>	  
+</div> <!-- end of header_wrapper -->
+
+<div id="Slide">
+<p style="font-size:20px; clear:both;">
+    Niềm tin cơ bản
+
+    Nền tảng của Tôn chỉ Công ty bao gồm 2 niềm tin cơ bản:
+    Tôn trọng con người
+    Ba niềm vui
+</p>
+    </div>
+
+<?php 
+include_once"layout/banner.php";
+include_once"layout/footer.php";
+?>
