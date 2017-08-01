7bb9215748a690e2805e3faaf739cf21189a4c82
thangkun3012 <thangkun3012@gmail.com>
2017-08-01 12:43:04 +0700

    Update footer.php

--- shop xe may/layout/footer.php
+++ shop xe may/layout/footer.php
@@ -2,9 +2,9 @@
 
 <div id="BHT_footer_wrapper">
 
-     <div id="BHT_footer">
+     <div>
     
-        Copyright © 2017 <a href="#">BHT SHOP</a> - Email: <a>thangkun3012@gmail.com</a>
+        <div id="BHT_footer">Copyright © 2017 <a  href="#">BHT SHOP</a> - Email: <a>thangkun3012@gmail.com</a></div>
     
     </div> <!-- end of tooplate_footer -->
 
