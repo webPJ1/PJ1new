1887fa44b46ce61d9914d9c385c274f1d90aa528
thangkun3012 <thangkun3012@gmail.com>
2017-08-09 18:24:45 +0700

    Update top.php

--- shop xe may/layout/top.php
+++ shop xe may/layout/top.php
@@ -1,18 +1,9 @@
-
-
-    <div id="BHT_header">
+<div id="BHT_header">
     
         <div id="site_title">
         
-        
-        	        	<img src="images/small-9395-597f22156d2bd.png"/>
-        
-        </div> <!-- end of site_title -->
-        <div id="header_phone_no">
 
-			<a style="color:#000>SĐT liên hệ:</a> <span>0989875471</span>
-        
-        </div>
-        
-        
-        
+        <a><img src="images/logo1.png"/></a>
+
+
+        </div> <!-- end of site_title -->
